c5fca898fc7028e067065788b0db40530a5e080b
Daniel Sef <danielsef@icloud.com>
2017-11-10 08:35:14 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
 export default {
   authProvider: "google",
-  apiKey: "AIzaSyBpMWFFZF2cwlxKe1z7YH6b893JVzwS7WM",
-  authDomain: "octopus-6d17d.firebaseapp.com",
-  databaseURL: "https://octopus-6d17d.firebaseio.com",
-  projectId: "octopus-6d17d",
-  storageBucket: "",
-  messagingSenderId: "44653455422"
+    apiKey: "AIzaSyBpMWFFZF2cwlxKe1z7YH6b893JVzwS7WM",
+    authDomain: "octopus-6d17d.firebaseapp.com",
+    databaseURL: "https://octopus-6d17d.firebaseio.com",
+    projectId: "octopus-6d17d",
+    storageBucket: "octopus-6d17d.appspot.com",
+    messagingSenderId: "44653455422"
+
 };
