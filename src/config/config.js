03753991b845ef310ca1d70556d253910435d38b
sak.ma.kettesben@gmail.com <danielsef@icloud.com>
2017-11-10 08:21:05 +0100

    config
    setup

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export default {
+  authProvider: "google",
+  apiKey: "AIzaSyBpMWFFZF2cwlxKe1z7YH6b893JVzwS7WM",
+  authDomain: "octopus-6d17d.firebaseapp.com",
+  databaseURL: "https://octopus-6d17d.firebaseio.com",
+  projectId: "octopus-6d17d",
+  storageBucket: "",
+  messagingSenderId: "44653455422"
+};
